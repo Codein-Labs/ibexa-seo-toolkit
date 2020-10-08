2b0e3dcac1b91c3fd5f2aaeea6da699d342c472c
Ahmed Eben Hassine <ahmedbhs123@gmail.com>
2020-10-08 17:08:45 +0200

    feat: mapping contextualized di configuration

--- bundle/DependencyInjection/EzPlatformSeoToolkitExtension.php
+++ bundle/DependencyInjection/EzPlatformSeoToolkitExtension.php
@@ -2,12 +2,12 @@
 
 namespace Codein\eZPlatformSeoToolkit\DependencyInjection;
 
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ConfigurationProcessor;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ContextualizerInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class EzPlatformSeoToolkitExtension.
@@ -22,14 +22,24 @@ final class EzPlatformSeoToolkitExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
+        $config = $this->processConfiguration($configuration, $configs);
+        $processor = new ConfigurationProcessor($container, $this->getAlias());
+
+        $processor->mapConfig(
+            $config,
+            static function ($scopeSettings, $currentScope, ContextualizerInterface $contextualize) {
+                foreach ($scopeSettings as $key => $value) {
+                    $contextualize->setContextualParameter($key, $currentScope, $value);
+                }
+            }
+        );
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
     }
 
     public function getAlias(): string
-     {
-         return self::EXTENSION_ALIAS;
-     }
+    {
+        return self::EXTENSION_ALIAS;
+    }
 }
