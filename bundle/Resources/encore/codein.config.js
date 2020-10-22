c8dd82188b6c0a4593da9697c1766b83185fcd62
Alex <dinath.contact@gmail.com>
2020-10-22 17:17:47 +0200

    build(react): Enable use of React

--- bundle/Resources/encore/codein.config.js
+++ bundle/Resources/encore/codein.config.js
@@ -1,26 +1,30 @@
-const path = require('path');
-const addJSEntries = require('./codein.js.config.js');
-const addCSSEntries = require('./codein.css.config.js');
+const path = require("path");
+const addJSEntries = require("./codein.js.config.js");
+const addCSSEntries = require("./codein.css.config.js");
 
 module.exports = (Encore) => {
-    Encore.reset();
-    Encore.setOutputPath('web/bundles/codein-ezplatformseotoolkit')
-        .setPublicPath('/bundles/codein-ezplatformseotoolkit')
-        .addExternals({
-            react: 'React',
-            'react-dom': 'ReactDOM'
-        })
-        .copyFiles({
-            from: path.resolve(__dirname,'../public/img'),
-            to: 'images/[name].[ext]'
-        })
-        .disableSingleRuntimeChunk()
-        .enableSassLoader()
+  Encore.reset();
+  Encore.setOutputPath("web/bundles/codein-ezplatformseotoolkit")
+    .setPublicPath("/bundles/codein-ezplatformseotoolkit")
+    .addExternals({
+      react: "React",
+      "react-dom": "ReactDOM",
+      "react-spring": "react-spring",
+      "styled-components": "styled-components",
+    })
+    .copyFiles({
+      from: path.resolve(__dirname, "../public/img"),
+      to: "images/[name].[ext]",
+    })
+    .disableSingleRuntimeChunk()
+    .enableSassLoader()
+    .enableReactPreset();
 
-    addJSEntries(Encore);
-    addCSSEntries(Encore);
-    const codeinSeoToolkitConfig = Encore.getWebpackConfig(); 
-    codeinSeoToolkitConfig.name = 'codein';
+  addJSEntries(Encore);
+  addCSSEntries(Encore);
 
-    return codeinSeoToolkitConfig;
+  const codeinSeoToolkitConfig = Encore.getWebpackConfig();
+  codeinSeoToolkitConfig.name = "codein";
+
+  return codeinSeoToolkitConfig;
 };
--- bundle/Resources/public/js/index.js
+++ bundle/Resources/public/js/index.js
@@ -1,1 +1,10 @@
-// Default entrypoint
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
