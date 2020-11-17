54f657a2fa17ce153b48b5c0bce4793a5db70357
Meryll Essig <meryll@essig.io>
2020-11-17 14:46:58 +0100

    feat(analysis): styling result lines

--- bundle/Resources/encore/codein.css.config.js
+++ bundle/Resources/encore/codein.css.config.js
@@ -4,5 +4,6 @@ module.exports = (Encore) => {
   Encore.addEntry("codein-ezplatform-seo-toolkit-css", [
     path.resolve(__dirname, "../public/scss/seo-menu.module.scss"),
     path.resolve(__dirname, "../public/scss/seo-loading.scss"),
+    path.resolve(__dirname, "../public/scss/seo-analysis.scss"),
   ]);
 };
--- bundle/Resources/public/js/modules/seo-menu/components/analysis/analysis.category.content.js
+++ bundle/Resources/public/js/modules/seo-menu/components/analysis/analysis.category.content.js
@@ -3,9 +3,17 @@ import React from "react";
 const AnalysisCategoryContent = (props) => {
   return (
     <>
-      <ul>
-        <li>...</li>
-      </ul>
+      <div className="analysis-content">
+        <p className="analysis-content__result analysis-content__result--high">
+          Texte d'analyse high score
+        </p>
+        <p className="analysis-content__result analysis-content__result--medium">
+          Texte d'analyse medium score
+        </p>
+        <p className="analysis-content__result analysis-content__result--low">
+          Texte d'analyse low score
+        </p>
+      </div>
     </>
   );
 };
