6834ff8d3132fe1d191a995636eecc3bdd3e404d
Meryll Essig <meryll@essig.io>
2020-11-19 17:49:59 +0100

    refactor(analysis): AnalyzeContentController now gets the right data

---
+++ bundle/Service/AnalyzeContentService.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types=1);
+
+namespace Codein\eZPlatformSeoToolkit\Service;
+
+use Codein\eZPlatformSeoToolkit\Analyzer\ContentPreviewParentAnalyzerService;
+use Codein\eZPlatformSeoToolkit\Analyzer\RichTextParentAnalyzerService;
+use Codein\eZPlatformSeoToolkit\Entity\ContentConfiguration;
+use Doctrine\ORM\EntityManager;
+use eZ\Publish\API\Repository\ContentTypeService;
+use eZ\Publish\Core\Repository\Values\ContentType\FieldDefinition;
+use EzSystems\EzPlatformRichText\eZ\FieldType\RichText\Value;
+use Codein\eZPlatformSeoToolkit\Helper\XmlValidator;
+use Codein\eZPlatformSeoToolkit\Model\ContentFields;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * Class AnalyzeContentService.
+ */
+final class AnalyzeContentService
+{
+
+    /** @var EntityManager $em */
+    private $em;
+
+    /** @var ContentTypeService $contentTypeService*/
+    private $contentTypeService;
+
+    /** @var ContentPreviewParentAnalyzerService $contentPreviewAnalyzer */
+    private $contentPreviewAnalyzer;
+
+    /** @var RichTextParentAnalyzerService $richTextAnalyzer */
+    private $richTextAnalyzer;
+
+    public function __construct(
+        EntityManager $em, 
+        ContentTypeService $contentTypeService, 
+        RichTextParentAnalyzerService $richTextAnalyzer,
+        ContentPreviewParentAnalyzerService $contentPreviewAnalyzer
+    ) {
+        $this->em = $em;
+        $this->contentTypeService = $contentTypeService;
+        $this->richTextAnalyzer = $richTextAnalyzer;
+        $this->contentPreviewAnalyzer = $contentPreviewAnalyzer;
+    }
+
+    public function buildResultObject(Request $request, ContentFields $contentFields) {
+        $contentType = $this->contentTypeService->loadContentTypeByIdentifier($contentFields->getContentTypeIdentifier());
+        $contentId = $contentFields->getContentId();
+
+        $result = [];
+
+        $data = $this->em->getRepository(ContentConfiguration::class)->findOneBy([
+            'contentId' => $contentId
+        ])->toArray();
+
+        foreach ($contentFields->getFields() as $field) {
+            $fieldDefinition = $contentType->getFieldDefinition($field->getFieldIdentifier());
+            $fieldDefinition = ($fieldDefinition) ?? new FieldDefinition(
+                [
+                    'fieldTypeIdentifier' => 'ezrichtext',
+                ]
+            );
+            if (false === XmlValidator::isXMLContentValid($field->getFieldValue())) {
+                throw new HttpException(400, \sprintf('Invalid xml value for field "%s".', $field->getFieldIdentifier()));
+            }
+            $fieldValue = new Value($field->getFieldValue());
+            $result = $this->richTextAnalyzer
+                ->analyze($fieldDefinition, $fieldValue, $data);
+            
+        }
+        $data = \array_merge($data, $contentFields->toArray());
+        $data['request'] = $request;
+        $resultContentPreview = $this->contentPreviewAnalyzer->analyze($data);
+        $result = \array_merge_recursive($result, $resultContentPreview);
+
+        return $result;
+    }
+}
