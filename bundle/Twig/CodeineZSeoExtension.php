0e7ee57198aa6428d6a0bcc951c2fd9b9fb6d0ae
Meryll Essig <meryll@essig.io>
2020-11-19 17:50:59 +0100

    feat(generic): twig function and providing more data to react app

--- bundle/Twig/CodeineZSeoExtension.php
+++ bundle/Twig/CodeineZSeoExtension.php
@@ -3,8 +3,11 @@
 namespace Codein\eZPlatformSeoToolkit\Twig;
 
 use Codein\eZPlatformSeoToolkit\Helper\SiteAccessConfigResolver;
+use eZ\Publish\API\Repository\Values\Content\Location;
 use Twig\Extension\AbstractExtension;
 use Twig\Extension\GlobalsInterface;
+use Twig\TwigFunction;
+use EzSystems\EzPlatformAdminUi\Siteaccess\SiteaccessResolver;
 
 /**
  * Class CodeineZSeoExtension.
@@ -14,23 +17,49 @@ final class CodeineZSeoExtension extends AbstractExtension implements GlobalsInt
     /** @var SiteAccessConfigResolver */
     private $siteAccessConfigResolver;
 
+    /** @var array */
+    private $siteAccessesByLanguage;
+
     /**
      * CodeineZSeoExtension constructor.
      * @param $configResolver
      */
-    public function __construct(SiteAccessConfigResolver $configResolver)
+    public function __construct(SiteAccessConfigResolver $configResolver, array $siteAccessesByLanguage)
     {
         $this->siteAccessConfigResolver = $configResolver;
+        $this->siteAccessesByLanguage = $siteAccessesByLanguage;
     }
 
     public function getGlobals(): array
     {
         $metas = $this->siteAccessConfigResolver->getParameterConfig('metas');
-        $codeineZseo = [
+        $codeinEzSeo = [
             'field_type_metas' => $metas['field_type_metas'],
             'default_metas' => $metas['default_metas'],
         ];
 
-        return ['codein_ezseo' => $codeineZseo];
+        return ['codein_ezseo' => $codeinEzSeo];
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('codein_siteaccesses_by_language', [$this, 'getSiteaccessesByLanguage']),
+        ];
+    }
+
+    /**
+     * Find potential siteaccesses for this language code
+     *
+     * @param string $languageCode
+     * @return string
+     */
+    public function getSiteaccessesByLanguage(string $languageCode): string
+    {
+        $siteAccesses = [];
+        if ($languageCode && array_key_exists($languageCode, $this->siteAccessesByLanguage)) {
+            $siteAccesses = $this->siteAccessesByLanguage[$languageCode];
+        }
+        return \json_encode($siteAccesses);
     }
 }
