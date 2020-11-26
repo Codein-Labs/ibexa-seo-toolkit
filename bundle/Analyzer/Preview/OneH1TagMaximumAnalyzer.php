6327bbc0da5cf5822dc5ffae0b3e21d567ecd803
Meryll Essig <meryll@essig.io>
2020-11-26 16:37:46 +0100

    feat(analysis): add OneH1TagMaximumAnalyzer

---
+++ bundle/Analyzer/Preview/OneH1TagMaximumAnalyzer.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Codein\eZPlatformSeoToolkit\Analyzer\Preview;
+
+use Codein\eZPlatformSeoToolkit\Analyzer\Preview\ContentPreviewAnalyzerInterface;
+use Codein\eZPlatformSeoToolkit\Service\AnalyzerService;
+
+/**
+ * Class CountWordService.
+ */
+final class OneH1TagMaximumAnalyzer implements ContentPreviewAnalyzerInterface
+{
+    /** @var \Codein\eZPlatformSeoToolkit\Service\AnalyzerService $as */
+    private $as;
+
+    private const CATEGORY = 'codein_seo_toolkit.analyzer.category.lisibility';
+
+
+    public function __construct(AnalyzerService $analyzerService)
+    {
+        $this->as = $analyzerService;
+    }
+
+    public function analyze(array $data): array
+    {
+        $selector = new \DOMXPath($data['previewHtml']);
+        $h1 = $selector->query('//h1');
+        $count = $h1->count();
+        $status = 'low';
+        if ($count == 1) {
+            $status = 'high';
+        }
+        $analysisData = [
+            'count' => $count
+        ];
+        return $this->as->compile(self::CATEGORY, $status, $analysisData);
+    }
+
+    public function support($data): bool
+    {
+        return true;
+    }
+}
