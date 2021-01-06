7779d6c6d840391f4a897d46335b44621f801f7e
Meryll Essig <meryll@essig.io>
2021-01-06 11:26:33 +0100

    fix(sitemap): URL Generator interface => https

--- bundle/Controller/SitemapController.php
+++ bundle/Controller/SitemapController.php
@@ -8,7 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Class SitemapController.
