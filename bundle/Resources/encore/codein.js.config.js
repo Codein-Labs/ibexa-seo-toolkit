01688df3dff8004baa8011ee592dbb0ec3d2d531
Meryll Essig <meryll@essig.io>
2021-03-01 16:31:16 +0100

    fix: webpack config update for translation handling

--- bundle/Resources/encore/codein.js.config.js
+++ bundle/Resources/encore/codein.js.config.js
@@ -1,7 +1,17 @@
-const path = require("path");
+const path = require('path');
+const fs = require('fs');
+const translationsPath = path.resolve('./web/assets/translations/');
+
+let jsEntries = [
+  path.resolve(__dirname, "../public/js/modules/seo-menu/index.js"),
+]
+
+fs.readdirSync(translationsPath).forEach((file) => {
+  if (file !== 'config.js' && path.extname(file) === '.js') {
+    jsEntries.push(path.resolve(translationsPath, file));
+  }
+});
 
 module.exports = (Encore) => {
-  Encore.addEntry("codein-ezplatform-seo-toolkit-js", [
-    path.resolve(__dirname, "../public/js/modules/seo-menu/index.js"),
-  ]);
+  Encore.addEntry("codein-ezplatform-seo-toolkit-js", jsEntries);
 };
