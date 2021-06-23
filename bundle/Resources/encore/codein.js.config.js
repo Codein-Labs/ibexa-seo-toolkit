40572679a0755507c2537af07cb673efba2bab9c
Rudy Chailly <rudy.chailly@codein.fr>
2021-06-23 11:40:59 +0200

    fix: translations path

--- bundle/Resources/encore/codein.js.config.js
+++ bundle/Resources/encore/codein.js.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const translationsPath = path.resolve('./web/assets/translations/');
+const translationsPath = path.resolve('./public/assets/translations/');
 
 let jsEntries = [
   path.resolve(__dirname, "../public/js/modules/seo-menu/index.js"),
