0e1a5b1b43111c6b778b8f725c38df7e37ebbafa
Ahmed EBEN HASSINE <ahmed.eben-hassine@codein.fr>
2021-09-22 20:20:22 +0200

    feat: add github action Static analysis workflow

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,13 +1,13 @@
-<?php
+<?php declare(strict_types=1);
 
-declare(strict_types=1);
+use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__.'/bundle')
-
+    ->in([__DIR__])
+    ->exclude('bundle/DoctrineMigrations')
 ;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,
@@ -19,6 +19,7 @@ return PhpCsFixer\Config::create()
         'phpdoc_align' => false,
         'phpdoc_separation' => false,
         'phpdoc_to_comment' => false,
+        'mb_str_functions' => true,
         'linebreak_after_opening_tag' => false,
         'cast_spaces' => false,
         'blank_line_after_opening_tag' => false,
@@ -36,16 +37,19 @@ return PhpCsFixer\Config::create()
         'strict_param' => true,
         'align_multiline_comment' => true,
         'array_indentation' => true,
-        'class_definition' => ['singleLine' => false],
+        'class_definition' => ['single_line' => false],
         'combine_consecutive_issets' => true,
         'combine_consecutive_unsets' => true,
         'declare_strict_types' => true,
         'no_alternative_syntax' => true,
-        'no_short_echo_tag' => true,
         'phpdoc_add_missing_param_annotation' => ['only_untyped' => false],
         'phpdoc_order' => true,
         'semicolon_after_instruction' => true,
-        'native_function_invocation' => true,
+        'native_function_invocation' => [
+            'include' => [NativeFunctionInvocationFixer::SET_COMPILER_OPTIMIZED ],
+            'scope' => 'namespaced',
+            'strict' => false, // or remove this line, as false is default value
+        ],
         'yoda_style' => true,
     ])
     ->setFinder($finder)
