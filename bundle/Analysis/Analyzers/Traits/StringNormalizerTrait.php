f18cd495e34adf03dbfca40d9718d6388a743209
Marc Teinturier <marc.teinturier@codein.fr>
2021-10-07 11:06:34 +0200

    Add analyzers traits for word count and string normalization

---
+++ bundle/Analysis/Analyzers/Traits/StringNormalizerTrait.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Codein\IbexaSeoToolkit\Analysis\Analyzers\Traits;
+
+use Codein\IbexaSeoToolkit\Service\AnalyzerService;
+
+trait StringNormalizerTrait
+{
+    public function normalizeString(?string $string): string
+    {
+        return \strtr(\mb_strtolower($string), AnalyzerService::ACCENT_VALUES);
+    }
+}
---
+++ bundle/Analysis/Analyzers/Traits/WordCountTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Codein\IbexaSeoToolkit\Analysis\Analyzers\Traits;
+
+use DOMDocument;
+
+trait WordCountTrait
+{
+    protected function getWordCount(DOMDocument $DOMDocument): int
+    {
+        $htmlText = strip_tags($DOMDocument->saveHTML());
+        $htmlText = html_entity_decode(preg_replace(['/\n/', '/\r/', '/\t/', '/\s+/'], [' ', ' ', ' ', ' '], $htmlText));
+
+        return str_word_count($htmlText);
+    }
+}
